947722b0c01d2246bd1b0afced564d6d2f0494cc
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-21 01:36:37 +0530

    Ran seeds for inserting sample data

---
+++ database/seeds/001-users.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {email: 'user_one@gmail.com', password: '1234', username: 'user_one', is_private: false},
+        {email: 'user_two@gmail.com', password: '1234', username: 'user_two', is_private: false},
+        {email: 'user_three@gmail.com', password: '1234', username: 'user_three', is_private: false}
+      ]);
+    });
+};
