4d1ddb059ac225ae99a2d4b524c0e7a299bb3955
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-22 21:57:10 +0530

    changed login POST endpoint to send user also

--- routers/auth-router/auth-router.js
+++ routers/auth-router/auth-router.js
@@ -29,7 +29,7 @@ router.post('/login', (req, res) => {
                 const token = generateToken(user)
                 res.status(200).json({
                     message: `Welcome ${user.username}!`,
-                    token,
+                    token, user
                 });
             } else {
                 res.status(401).json({ message: 'You shall not pass' });
